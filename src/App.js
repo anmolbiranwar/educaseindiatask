3425201543649c39b669bd44215fd60b404cfc9d
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2024-02-15 12:00:12 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import LandingPage from './Components/LandingPage';
+import LoginPage from './Components/LoginPage';
+import SignupPage from './Components/SignupPage';
+import ProfilePage from './Components/ProfilePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/signup" element={<SignupPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path='/profile' element={<ProfilePage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
